2fcca2c5c46bde65d90e6150c785368d78075882
teejay6 <46007570+teejay6@users.noreply.github.com>
2019-09-09 17:59:17 -0700

    Add files via upload

---
+++ Turn_Off_All_LEDs.ino
@@ -0,0 +1,31 @@
+
+// Configured for Arduino Due
+// Configured for LPD8806 LED strip from Adafruit
+// Sketch Modifications by TomJ
+
+// Include FastLED Library
+#include <FastLED.h>
+
+// How many LEDs in your strip?
+#define NUM_LEDS 32
+
+// LPD8806 LED chipsets are SPI based (four wires - data, clock,
+// ground, and power), need to define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 3
+#define CLOCK_PIN 13
+
+//Correct LPD8806 for Color Order
+#define COLOR_ORDER GRB
+
+// Define the array of LEDs
+CRGB leds[NUM_LEDS];
+
+void setup() { 
+	// LPD8806 LEDs arrangement.    
+	FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+}
+
+void loop() {
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+}
